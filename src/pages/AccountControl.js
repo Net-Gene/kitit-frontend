d0bd5c2e48497b40fe81645d2243b94afb405f5a
jhalmeko <joel.halmekoski@edu.lapinamk.fi>
2024-11-13 20:12:23 +0200

    AccountControl/style edit username/password commit

--- src/pages/AccountControl.js
+++ src/pages/AccountControl.js
@@ -1,9 +1,76 @@
 import React from 'react';
 
+import '../styles/AccountControl.css';
+
+import blank_user_image from '../assets/blank user.png';
+import edit_pen_icon from '../assets/edit_pen_icon.png';
+import trash_can_icon from '../assets/trash_can_icon.png';
+import back_arrow from '../assets/back_arrow.png';
+
 const AccountControl = () => {
+
+  const [username, setUsername] = useState(""); // State username arvon talletukseen
+
+  // input muutos handler
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+  
+  const [password, setPassword] = useState(""); // State password arvon talletukseen
+
+  // input muutos handler
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
   return (
-    <div>
-      <h1>Account Control Site</h1>
+    // Tilinhallinta sivu
+    <div class ="accountControl">
+
+      {/*Takaisin nappi*/}
+      <div class ="back-button">
+        {/*Linkki pääsivulle, jossa on nuolikuvake*/}
+        <a href="/">
+          <img src={back_arrow} alt="back_arrow" class="back_arrow-img"></img>
+        </a>
+      </div>
+      
+      {/*Käyttäjänimen edit osio*/}
+      <div class ="edit_username_header">
+        <h1>
+          Edit Username
+        </h1>
+      </div>
+      <div class ="edit_username_textfield">
+        {/*Tekstikenttä*/}
+        <input 
+          type="text" 
+          value={username} 
+          onChange={handleUsernameChange} 
+          placeholder="Enter new username" 
+          className="username-input"
+        />
+        <img src={edit_pen_icon} alt="edit_pen_icon" class="edit_pen_icon-img"></img>
+      </div>
+
+      {/*Salasanan edit osio*/}
+      <div class ="edit_password_header">
+        <h1>
+          Edit Password
+        </h1>
+      </div>
+      <div class ="edit_password_textfield">
+        {/*Tekstikenttä*/}
+        <input 
+          type="text" 
+          value={password} 
+          onChange={handlePasswordChange} 
+          placeholder="Enter new password" 
+          className="password-input"
+        />
+        <img src={edit_pen_icon} alt="edit_pen_icon" class="edit_pen_icon-img"></img>
+      </div>
+
     </div>
 
   );
