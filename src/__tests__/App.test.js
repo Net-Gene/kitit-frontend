bd6a2c9ea841962c6f4bdbb264d6d29116d45a88
Dorian <dorian.boussakine@gmail.com>
2024-11-24 01:19:19 +0200

    Päivitä testi vastaamaan oikeaa sovelluskomponentissa renderöityä tekstiä

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,10 +1,8 @@
-// App.test.js
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom'; // Kääri sovellus MemoryRouterilla simuloidaksesi reititystä
 import App from '../App';
-import { act } from 'react';  // Import act from 'react'
 
-test('renders learn react link on home page', () => {
-  render(<App />);  // Remove MemoryRouter
+test('renders correct text on the home page', () => {
+  render(<App />);
+  screen.debug();  // Tämä tulostaa renderoidun tulosteen konsoliin
+  expect(screen.getByText(/Kirjautuminen/i)).toBeInTheDocument();
 });
-
